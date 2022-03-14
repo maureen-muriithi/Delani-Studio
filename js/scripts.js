a8adb83cde61a3c168b435159d4cad0e8b5589c7
Maureen-Muriithi <moh2wanja@gmail.com>
2022-03-14 03:37:41 +0300

    Add hover effect on portfolio images

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,81 @@
 $(document).ready(function(){
+    // toggle function on the 'What we do" section, to display description
     $("#design").click(function(){
         $("#designInfo").toggle();
         $("#designImage").toggle();
-    })
+    });
     
     $("#development").click(function(){
         $("#devInfo").toggle();
         $("#devImage").toggle();
-    })
+    });
     
     $("#productManagement").click(function(){
         $("#productInfo").toggle();
         $("#productImage").toggle();
-    })
+    });
 
-   
-    
+    $("#prj-work1").hover(function(){
+        $("#projectName1").addClass("project-hover")
+        
+    }, function(){
+        $("#projectName1").removeClass("project-hover")
+    });
 
-    // $ ("img#workImage1").hover(function(){
-    //     $("img#workImage1").text("p#projectName1");
-    //     $("img#workImage1").hover(function(){
-    //         }, function(){
-    //             $("p#projectName1");
-    //     });
-    // });
-})
+    // Hover function on the portfolio section
+    $("#prj-work2").hover(function(){
+        $("#projectName2").addClass("project-hover")
+        
+    }, function(){
+        $("#projectName2").removeClass("project-hover")
+    });
+
+    $("#prj-work3").hover(function(){
+        $("#projectName3").addClass("project-hover")
+        
+    }, function(){
+        $("#projectName3").removeClass("project-hover")
+    });
+
+    $("#prj-work4").hover(function(){
+        $("#projectName4").addClass("project-hover")
+        
+    }, function(){
+        $("#projectName4").removeClass("project-hover")
+    });
+
+    $("#prj-work5").hover(function(){
+        $("#projectName5").addClass("project-hover")
+        
+    }, function(){
+        $("#projectName5").removeClass("project-hover")
+    });
+
+    $("#prj-work6").hover(function(){
+        $("#projectName6").addClass("project-hover")
+        
+    }, function(){
+        $("#projectName6").removeClass("project-hover")
+    });
+
+    $("#prj-work7").hover(function(){
+        $("#projectName7").addClass("project-hover")
+        
+    }, function(){
+        $("#projectName7").removeClass("project-hover")
+    });
+
+    $("#prj-work8").hover(function(){
+        $("#projectName8").addClass("project-hover")
+        
+    }, function(){
+        $("#projectName8").removeClass("project-hover")
+    });
+});
 
 
 
-$('#formInfo').submit(function () {
+$('#formInfo').submit(function() {
     // e.preventDefault()
     var userName = $('#inputName').val();
     var userEmail = $('#inputEmail').val();
